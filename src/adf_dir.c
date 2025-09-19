7e60a1cecbe0032bc5e07341bb7e82207de8fab4
t-m <tomas.wolak@gmail.com>
2025-09-19 17:21:06 +0200

    adfGetRDirEnt: minor refactoring.
    Condition changed (reversed) to cut one level of (unnecessary)
    indentation.

--- src/adf_dir.c
+++ src/adf_dir.c
@@ -166,60 +166,61 @@ struct AdfList * adfGetRDirEnt( const struct AdfVolume * const  vol,
     hashTable = parent.hashTable;
     cell = head = NULL;
     for ( int i = 0 ; i < ADF_HT_SIZE ; i++ ) {
-        if ( hashTable[ i ] != 0 ) {
-             entry = ( struct AdfEntry * ) malloc( sizeof(struct AdfEntry) );
-             if ( ! entry ) {
-                 adfFreeDirList( head );
-                 adfEnv.eFct( "%s: malloc", __func__ );
-                 return NULL;
-             }
+        if ( hashTable[ i ] == 0 )
+            continue;
 
-             if ( adfEntryRead( vol, hashTable[ i ], entry, &entryBlk ) != ADF_RC_OK ) {
-                 free( entry );
-                 adfFreeDirList( head );
-                 return NULL;
-             }
+        entry = (struct AdfEntry *) malloc( sizeof( struct AdfEntry ) );
+        if ( ! entry ) {
+            adfFreeDirList( head );
+            adfEnv.eFct( "%s: malloc", __func__ );
+            return NULL;
+        }
+
+        if ( adfEntryRead( vol, hashTable[ i ], entry, &entryBlk ) != ADF_RC_OK ) {
+            free( entry );
+            adfFreeDirList( head );
+            return NULL;
+        }
 	
-             if ( head == NULL )
-                 head = cell = adfListNewCell( NULL, (void *) entry );
-             else
-                 cell = adfListNewCell( cell, (void *) entry );
-             if ( cell == NULL ) {
-                 free( entry );
-                 adfFreeDirList( head );
-                 return NULL;
-             }
+        if ( head == NULL )
+            head = cell = adfListNewCell( NULL, (void *) entry );
+        else
+            cell = adfListNewCell( cell, (void *) entry );
+        if ( cell == NULL ) {
+            free( entry );
+            adfFreeDirList( head );
+            return NULL;
+        }
 
-             if ( recurs && entry->type == ADF_ST_DIR )
-                 cell->subdir = adfGetRDirEnt( vol, entry->sector, recurs );
+        if ( recurs && entry->type == ADF_ST_DIR )
+            cell->subdir = adfGetRDirEnt( vol, entry->sector, recurs );
 
-             /* same hashcode linked list */
-             ADF_SECTNUM nextSector = entryBlk.nextSameHash;
-             while ( nextSector != 0 ) {
-                 entry = ( struct AdfEntry * ) malloc( sizeof(struct AdfEntry) );
-                 if ( ! entry ) {
-                     adfFreeDirList( head );
-                     adfEnv.eFct( "%s: malloc", __func__ );
-                     return NULL;
-                 }
+        /* same hashcode linked list */
+        ADF_SECTNUM nextSector = entryBlk.nextSameHash;
+        while ( nextSector != 0 ) {
+            entry = (struct AdfEntry *) malloc( sizeof( struct AdfEntry ) );
+            if ( ! entry ) {
+                adfFreeDirList( head );
+                adfEnv.eFct( "%s: malloc", __func__ );
+                return NULL;
+            }
 
-                 if ( adfEntryRead( vol, nextSector, entry, &entryBlk ) != ADF_RC_OK ) {
-                     free( entry );
-                     adfFreeDirList( head );
-                     return NULL;
-                 }
+            if ( adfEntryRead( vol, nextSector, entry, &entryBlk ) != ADF_RC_OK ) {
+                free( entry );
+                adfFreeDirList( head );
+                return NULL;
+            }
 	
-                 cell = adfListNewCell( cell, (void *) entry );
-                 if ( cell == NULL ) {
-                     adfFreeDirList( head );
-                     return NULL;
-                 }
+            cell = adfListNewCell( cell, (void *) entry );
+            if ( cell == NULL ) {
+                adfFreeDirList( head );
+                return NULL;
+            }
 				 
-                 if ( recurs && entry->type == ADF_ST_DIR )
-                     cell->subdir = adfGetRDirEnt( vol, entry->sector, recurs );
+            if ( recurs && entry->type == ADF_ST_DIR )
+                cell->subdir = adfGetRDirEnt( vol, entry->sector, recurs );
 				 
-                 nextSector = entryBlk.nextSameHash;
-             }
+            nextSector = entryBlk.nextSameHash;
         }
     }
 
